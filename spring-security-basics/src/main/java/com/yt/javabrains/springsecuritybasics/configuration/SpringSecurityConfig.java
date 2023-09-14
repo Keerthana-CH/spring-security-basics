3eece5e896d7376544b3c77a20b0f4159550ce1e
Keerthana-CH <110730952+Keerthana-CH@users.noreply.github.com>
2023-09-14 08:33:15 -0700

    authorization

--- spring-security-basics/src/main/java/com/yt/javabrains/springsecuritybasics/configuration/SpringSecurityConfig.java
+++ spring-security-basics/src/main/java/com/yt/javabrains/springsecuritybasics/configuration/SpringSecurityConfig.java
@@ -3,10 +3,13 @@ package com.yt.javabrains.springsecuritybasics.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.function.Function;
 
@@ -15,8 +18,8 @@ public class SpringSecurityConfig {
 
     @Bean
     public InMemoryUserDetailsManager configureUserDetails() {
-        UserDetails userDetails1 = createNewUser("user11","password1","USER");
-        UserDetails userDetails2 = createNewUser("user2","password2","USER");
+        UserDetails userDetails1 = createNewUser("user1","password1","USER");
+        UserDetails userDetails2 = createNewUser("admin1","password1","ADMIN");
         UserDetails userDetails3 = createNewUser("user3","password3","USER");
         return new InMemoryUserDetailsManager(userDetails1,userDetails2,userDetails3);
     }
@@ -35,5 +38,18 @@ public class SpringSecurityConfig {
     @Bean
     public static BCryptPasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity.authorizeHttpRequests()
+                .requestMatchers("/admin").hasRole("ADMIN")
+                .requestMatchers("/user").hasAnyRole("USER","ADMIN")
+                .requestMatchers("/").permitAll();
+
+        httpSecurity.formLogin(Customizer.withDefaults());
+
+        return httpSecurity.build();
     }
 }
--- spring-security-basics/src/main/java/com/yt/javabrains/springsecuritybasics/resources/ResourceEx.java
+++ spring-security-basics/src/main/java/com/yt/javabrains/springsecuritybasics/resources/ResourceEx.java
@@ -5,11 +5,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/")
 public class ResourceEx {
 
-    @GetMapping
-    public String getWelcomePage(){
-        return ("<h1>Welcome</h1>");
+    @GetMapping("/")
+    public String home(){
+        return ("<h1>Welcome All</h1>");
+    }
+
+    @GetMapping("/user")
+    public String user(){
+        return ("<h1>Welcome User</h1>");
+    }
+
+    @GetMapping("/admin")
+    public String admin(){
+        return ("<h1>Welcome Admin</h1>");
     }
 }
